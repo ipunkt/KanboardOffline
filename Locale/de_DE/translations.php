296e45669f4543d31269a97d34f678ca3e36f4b4
andrei <av@ipunkt.biz>
2019-07-19 16:10:14 +0200

    WIP translation in German for settings page;

--- Locale/de_DE/translations.php
+++ Locale/de_DE/translations.php
@@ -1,5 +1,11 @@
 <?php
 
 return array(
-
+    'Settings' => 'Einstellungen',
+    'Offline settings' => 'Offline Einstellungen',
+    'Snake game' => 'Schlangenspiel',
+    'Enable "Snake" game, while waiting for the connection.' => 'Aktivieren Sie "Schlangenspiel" um zu spielen, während Sie auf die Verbindung warten.',
+    'Check connection status on page load.' => 'Überprüfen den Verbindungsstatus beim Laden der Seite.',
+    'Store and attempt to remake requests which fail while the connection is down.' => 'Anfragen speichern, die bei unterbrochener Verbindung fehlschlagen, und versuchen, sie erneut zu erstellen.',
+    'Monitor AJAX requests to help decide if we have a connection.' => 'Überwachen AJAX-Anfragen, ob eine Verbindung besteht.'
 );
