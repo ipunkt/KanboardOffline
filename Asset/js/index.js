3fe65211dad67684549387053ba1e33d303d6e86
andrei <av@ipunkt.biz>
2019-07-19 16:11:33 +0200

    WIP Offline.options, javascript;

--- Asset/js/index.js
+++ Asset/js/index.js
@@ -1,16 +1,24 @@
-import 'offline.min';
+function reqListener () {
+    console.log(this.responseText);
+}
+reqListener();
+let req = new XMLHttpRequest();
+req.onload = function() {
+  let check_on_load = this.responseText;
+  console.log(check_on_load);
+  // alert(check_on_load);
+};
+// req.open('get', 'offline-settings-page.php', true);
+// req.send();
 
 Offline.options = {
-    checkOnLoad: false,
-    interceptRequests: true,
-    requests: true,
     game: true,
+    checkOnLoad: false,
+    requests: true,
+    interceptRequests: true,
     reconnect: {
-        initialDelay: 5,
-        delay: 15
+        initialDelay: 15,
+        delay: 10
     }
 };
-
-// Offline.on('up');
-
 
